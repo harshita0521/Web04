217113c4a0bdf9d0c5d3a7a019f9dabca8d090d4
harshita0521 <harshita721721@gmail.com>
2026-08-11 17:57:22 +0530

    Add files via upload

--- jquery-effects.js
+++ jquery-effects.js
@@ -4,4 +4,9 @@
     $(".menu").click(function(){
         $(".nav-list").slideToggle(400);
     });
+
+    // Read More //
+    $(".read-btn").click(function(){
+        $(".more-text").slideToggle(500);
+    });
 })
